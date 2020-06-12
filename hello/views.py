6381eb7aa391a696abcb1767d4712b6ac6e32a77
atushi0919 <hatake0919@yahoo.co.jp>
2020-06-12 14:07:22 +0900

    Added record operation function

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from .models import Friend
-from .forms import HelloForm
+from .forms import FriendForm
 
 
 def index(request):
@@ -13,23 +13,41 @@ def index(request):
             }
     return render(request, "hello/index.html", params)
 
-# create model
+
 def create(request):
-    params = {
-            "title": "Hello",
-            "form": HelloForm(),
-    }
     if request.method == "POST":
-        name = request.POST["name"]
-        mail = request.POST["mail"]
-        gender = "gender" in request.POST
-        age = request.POST["age"]
-        birth = request.POST["birthday"]
-        friend = Friend(name=name,
-                        mail=mail,
-                        gender=gender,
-                        age=age,
-                        birthday=birth)
+        obj = Friend()
+        friend = FriendForm(request.POST, instance=obj)
         friend.save()
         return redirect(to="/hello")
+    params = {
+            "title": "Hello",
+            "form": FriendForm(),
+    }
     return render(request, "hello/create.html", params)
+
+
+def edit(request, num):
+    obj = Friend.objects.get(id=num)
+    if request.method == "POST":
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to="/hello")
+    params = {
+            "title": "Hello",
+            "id": num,
+            "form": FriendForm(instance=obj),
+    }
+    return render(request, "hello/edit.html", params)
+
+def delete(request, num):
+    friend = Friend.objects.get(id=num)
+    if request.method == "POST":
+        friend.delete()
+        return redirect(to="/hello")
+    params = {
+            "title": "Hello",
+            "id": num,
+            "obj": friend,
+    }
+    return render(request, "hello/delete.html", params)
